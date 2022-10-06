213c695411b2df401996c3c86ab20d253f87f2ce
Jeremy Jerez <110129245+JeremyJerez@users.noreply.github.com>
2022-10-05 20:46:15 -0500

    Create seasons.py
    I do not support plagiarism, this it"s an example of my basic approach to the problem
    based on the course teachings.
    
    pip install inflect
    
    use it in the same folder where there are the files to be tested

---
+++ seasons.py
@@ -0,0 +1,35 @@
+# Python_Intro
+# Problem Set 7
+#A series of exercises for CS50 hands-on projects
+"""
+This one's my approach to the "Seasons of Love" problem
+"""
+from datetime import date
+import sys
+import re
+import inflect
+
+X = inflect.engine()
+
+def main():
+    Birthday = input("Date of Birth: ")
+    try:
+        year, month, day = check_Birthday(Birthday)
+    except:
+        sys.exit("Invalid date")
+    BirthDate = date(int(year), int(month), int(day))
+    ActualDate = date.today()
+    Diff = ActualDate - BirthDate
+    Total_Minutes = Diff.days * 24 * 60
+    Output = X.number_to_words(Total_Minutes, andword='')
+    print(Output.capitalize() + " minutes")
+
+
+def check_Birthday(Birthday):
+    if re.search(r"^[0-9]{4}-[0-9]{2}-[0-9]{2}$", Birthday):
+        year, month, day = Birthday.split("-")
+        return year, month, day
+
+
+if __name__ == "__main__":
+    main()
